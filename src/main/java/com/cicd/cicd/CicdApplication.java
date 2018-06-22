2d290661e1fb3bc97e5d00252ac015936c7fe58d
zhuran <zhuran@flashhold.com>
2018-06-22 10:55:16 +0800

    test

--- src/main/java/com/cicd/cicd/CicdApplication.java
+++ src/main/java/com/cicd/cicd/CicdApplication.java
@@ -9,4 +9,5 @@ public class CicdApplication {
     public static void main(String[] args) {
         SpringApplication.run(CicdApplication.class, args);
     }
+
 }
---
+++ src/main/java/com/cicd/cicd/controller/CICD.java
@@ -0,0 +1,18 @@
+package com.cicd.cicd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by zhuran on 2018/6/21 0021
+ */
+@Controller
+public class CICD {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String cicd() {
+        return "test";
+    }
+}
